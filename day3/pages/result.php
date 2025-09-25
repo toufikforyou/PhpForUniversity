8a8fd76fc88adc283c728b4144ed65551987f2f8
MH TOUFIK <toufik@sopnolikhi.com>
2025-09-25 15:32:19 +0600

    Revamp result page with styling and structure

--- day3/pages/result.php
+++ day3/pages/result.php
@@ -1,14 +1,17 @@
-<table border="1" align="center" width="800">
-    <tr>
-        <th>Name</th>
-        <th>Mobile</th>
-        <th>Email</th>
-    </tr>
-    <?php foreach ($results as $result) { ?>
-        <tr>
-            <td><?php echo $result['course'] ?></td>
-            <td><?php echo $result['course_code'] ?></td>
-            <td><?php echo $result['grade'] ?></td>
+<body style="background: black">
+    <h1 align="center" style="color:white;">My Result Grade Sheet</h1>
+    <table border="1" align="center" width="800">
+        <tr style="background: chartreuse">
+            <th>Course</th>
+            <th>Course Code</th>
+            <th>Grade</th>
         </tr>
+        <?php foreach ($results as $result) { ?>
+            <tr style="background: aquamarine">
+                <td><?php echo $result['course'] ?></td>
+                <td><?php echo $result['course_code'] ?></td>
+                <td><?php echo $result['grade'] ?></td>
+            </tr>
         <?php } ?>
-</table>
+    </table>
+</body>
