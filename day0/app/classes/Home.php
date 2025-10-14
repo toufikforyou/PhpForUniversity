696ffb3e124c44d09f6c6c0aa3df2bbfc63b1015
MH TOUFIK <toufik@sopnolikhi.com>
2025-10-14 15:14:17 +0600

    Day 0 Hello World

---
+++ day0/app/classes/Home.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\classes;
+
+class Home
+{
+    public $message;
+    public function __construct()
+    {
+        $this->message = "Hello World!";
+    }
+
+    public function index(){
+        echo $this->message;
+    }
+
+}
---
+++ day0/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once './vendor/autoload.php';
+
+use App\classes\Home;
+
+$obj = new Home();
+
+$obj->index();
